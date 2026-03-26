85aa2bf777c9c7a5de94c9534ff5ca9fd435b7b9
Deryx E. Scott <deryxescott@gmail.com>
2026-03-26 17:20:31 -0400

    Create and add Header, Footer, Main, and Nav components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h3>Doormat Navigation</h3>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Menu</a>
+                </li>
+                <li>
+                    <a href="#">Reservations</a>
+                </li>
+                <li>
+                    <a href="#">Order Online</a>
+                </li>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+            </ul>
+            <h3>Contact</h3>
+            <h3>Social Media Links</h3>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './components/Nav';
+
+const Header = () => {
+    return (
+        <header>
+            <img src="assets" alt="Little Lemon Logo" />
+            <Nav />
+        </header>
+    );
+}
+
+export default Header;
